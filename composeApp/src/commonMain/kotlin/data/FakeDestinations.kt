3f1fa612c7b96262b12fa2111d1fcf3ce2e6e5ff
ZeCarlos <zecarlosf07@gmail.com>
2024-11-21 12:41:08 -0500

    proyect

--- composeApp/src/commonMain/kotlin/data/FakeDestinations.kt
+++ composeApp/src/commonMain/kotlin/data/FakeDestinations.kt
@@ -72,7 +72,26 @@ object FakeDestinations {
                 price = "$18",
                 type = "Person",
                 category = FakeCategories.categories[3],
-                image = FakeImages.randomImageUrls(2),
+                image = FakeImages.randomImageUrls(3),
+                dates = arrayListOf("20 Dec - 24 Dec 2024", "25 Dec - 26 Dec 2024", "27 Dec - 30 Dec 2024"),
+                meetingPoints = arrayListOf("Serang", "Baranga", "Manchester", "Folio"),
+                facilities = arrayListOf("Transport", "Simaksi", "Coffee Break", "Meals during trekking","Camping tents", "P3K", "Officially recognized mountain guide", "Guide during trekking", "Folio"),
+                via = "Lahore",
+                estimation = "1 Day"
+            )
+        )
+        add(
+            Destination(
+                id = 1,
+                thumbnail = FakeImages.randomThumbnailUrl(),
+                title = "Rinjani Mountain",
+                description = "The mighty Rinjani mountain of Gunung Rinjani is a massive volcano which towers over the island of Lombok. A climb to the top is one of the most exhilarating experiences you can have in Indonesia. At 3,726 meters tall, Gunung Rinjani is the second highest mountain in Indonesia...",
+                rating = 2.7f,
+                location = "Rinjani, Indonesia",
+                price = "$18",
+                type = "Person",
+                category = FakeCategories.categories[4],
+                image = FakeImages.randomImageUrls(3),
                 dates = arrayListOf("20 Dec - 24 Dec 2024", "25 Dec - 26 Dec 2024", "27 Dec - 30 Dec 2024"),
                 meetingPoints = arrayListOf("Serang", "Baranga", "Manchester", "Folio"),
                 facilities = arrayListOf("Transport", "Simaksi", "Coffee Break", "Meals during trekking","Camping tents", "P3K", "Officially recognized mountain guide", "Guide during trekking", "Folio"),
